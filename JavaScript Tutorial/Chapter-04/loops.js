c3da00b019a84cc3f692d5d105d1ea41a747653a
Kartikay Gupta <guptakartikay12@gmail.com>
2025-03-13 23:56:46 +0530

    Working on JavaScript

--- JavaScript Tutorial/Chapter-04/loops.js
+++ JavaScript Tutorial/Chapter-04/loops.js
@@ -10,3 +10,34 @@ for(let i=n; i<=n*10; i=i+n){
 for(let count=1; count<=10; count++){
     console.log(count*n);
 }
+
+// Favourite movie
+
+const favMov = "Avengers";
+let guess = prompt("Enter your Fav Movie");
+
+while(guess != favMov){
+    if(guess == "quit"){
+        console.log("you quit!");
+        break;
+    }
+    
+    guess = prompt("Wrong guess,try again!");
+}
+
+if(guess == favMov){
+    console.log("Congrats!!");
+}
+
+// Loop with Arrays
+
+let fruits = ["mango","apple","banana","litchi","orange"];
+fruits.push("strawberry");
+
+for(let i=0; i < fruits.length; i++){
+    console.log(i,fruits[i]);
+}
+
+for( let i = fruits.length-1; i>=0; i=i-2){
+    console.log(i,fruits[i]);
+}
